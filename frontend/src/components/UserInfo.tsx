45034088709e0913b4892d63a8ec91b2a2bd9da5
mbecker20 <becker.maxh@gmail.com>
2022-03-11 22:45:09 -0800

    simplify

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -1,21 +1,12 @@
 import { Component, Match, Show, Switch } from "solid-js";
+import { getAuthProvider } from "../util/helpers";
 import Flex from "./util/layout/Flex";
 import Grid from "./util/layout/Grid";
 
 const UserInfo: Component<{ user: User; logout: () => void }> = (p) => {
   return (
     <Grid style={{ "font-size": "2rem" }}>
-      <Flex>
-        <div>provider:</div>
-        <Switch>
-          <Match when={p.user.githubID}>
-            <div>Github</div>
-          </Match>
-          <Match when={p.user.googleID}>
-            <div>Google</div>
-          </Match>
-        </Switch>
-      </Flex>
+      <div>provider: {getAuthProvider(p.user)}</div>
       <Flex alignItems="center">
         <div>username: {p.user.username}</div>
         <Show when={p.user.avatar}>
--- frontend/src/util/helpers.ts
+++ frontend/src/util/helpers.ts
@@ -1,3 +1,8 @@
 export function combineClasses(...classes: (string | undefined)[]) {
   return classes.filter((c) => (c ? true : false)).join(" ");
 }
+
+export function getAuthProvider(user: User) {
+  if (user.githubID) return "Github";
+  else if (user.googleID) return "Google"
+}
