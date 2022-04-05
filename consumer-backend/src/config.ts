4ab8b1cb85946f831db922dac136aec2d1a50575
mbecker20 <becker.maxh@gmail.com>
2022-04-05 05:54:34 -0700

    add auth url to env

--- consumer-backend/src/config.ts
+++ consumer-backend/src/config.ts
@@ -4,5 +4,5 @@ export const MONGO_URL = getStringFromEnv(
   "MONGO_URL",
   "mongodb://localhost:27017/oauth"
 );
-export const AUTH_URL = "http://localhost:2020/auth_jwt";
+export const AUTH_URL = getStringFromEnv("AUTH_URL", "http://localhost:2020/auth_jwt");
 export const PORT = 3000;
