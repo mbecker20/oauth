21696dee5a71ec835677760fe159f7db54d77108
mbecker20 <becker.maxh@gmail.com>
2022-04-05 05:10:11 -0700

    fix frontend route

--- auth-backend/src/routes/frontend.ts
+++ auth-backend/src/routes/frontend.ts
@@ -5,6 +5,10 @@ import fastifyStatic from "fastify-static";
 const frontend = fp((app: FastifyInstance, _: {}, done: () => void) => {
 	app.register(fastifyStatic, {
     root: "/frontend",
+    wildcard: false,
+  });
+  app.get("/*", (_, res) => {
+    res.sendFile("index.html");
   });
 	done();
 })
