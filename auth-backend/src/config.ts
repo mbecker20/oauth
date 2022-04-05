6e0a55f0bcd07542164b32d40027a7f9efbac732
mbecker20 <becker.maxh@gmail.com>
2022-04-04 17:35:59 -0700

    secrets config

--- auth-backend/src/config.ts
+++ auth-backend/src/config.ts
@@ -3,5 +3,5 @@ import { getNumberFromEnv, getStringFromEnv, readJSONFile } from "./helpers";
 export const MONGO_URL = getStringFromEnv("MONGO_URL", "mongodb://localhost:27017/oauth")
 export const PORT = getNumberFromEnv("PORT", 2020);
 export const HOST = `http://localhost:${PORT}`;
-export const SECRETS = readJSONFile("secrets.json");
+export const SECRETS = readJSONFile("/secrets/secrets.json");
 export const SALT_ROUNDS = 8;
