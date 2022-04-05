619ee2b98203342a064aab2cc274c8462092d24c
mbecker20 <becker.maxh@gmail.com>
2022-04-04 23:25:28 -0700

    fix frontend path

--- consumer-backend/src/routes/frontend.ts
+++ consumer-backend/src/routes/frontend.ts
@@ -1,11 +1,10 @@
 import { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
 import fastifyStatic from "fastify-static";
-import { resolve } from "path";
 
 const frontend = fp((app: FastifyInstance, _: {}, done: () => void) => {
   app.register(fastifyStatic, {
-    root: resolve("../consumer-frontend/build"),
+    root: "/frontend",
   });
   done();
 });
