c0496401ebc8df175483939cd65b515ecee6a3c1
mbecker20 <becker.maxh@gmail.com>
2022-04-05 05:42:50 -0700

    URL=location.origin

--- auth-frontend/src/index.tsx
+++ auth-frontend/src/index.tsx
@@ -1,16 +1,19 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './components/App/App';
-import Client from './util/client';
-import { getRedirectTo } from './util/helpers';
-import makeNotifications from './components/util/notification/Notifications';
+import "./index.css";
+import App from "./components/App/App";
+import Client from "./util/client";
+import { getRedirectTo } from "./util/helpers";
+import makeNotifications from "./components/util/notification/Notifications";
 
-export const URL = location.href.slice(0, location.href.length - 1);
+export const URL = location.origin;
 export const client = new Client(URL);
 export const redirectTo = getRedirectTo();
 
 export const { Notifications, pushNotification } = makeNotifications();
 
-render(() => [<App />, <Notifications />], document.getElementById('root') as HTMLElement);
+render(
+  () => [<App />, <Notifications />],
+  document.getElementById("root") as HTMLElement
+);
--- consumer-frontend/src/index.tsx
+++ consumer-frontend/src/index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./components/App/App";
 import Client from "./util/client";
 
-export const URL = location.href.slice(0, location.href.length - 1);
+export const URL = location.origin;
 export const AUTH_REDIRECT = "http://localhost:2020/?redirect=consumer";
 export const client = new Client(URL);
 
